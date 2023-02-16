9c5209300f7e38d807ba0f88b959be7d1f9b25cc
MarioACo <halo1232100@gmail.com>
2023-02-16 15:55:53 -0600

    Se agrego bootstrap

--- controller/controller_info.js
+++ controller/controller_info.js
@@ -1,26 +1,24 @@
+
+//funcion que valida que los datos esten agregados
 function validate_info(){
+    //valida si los input del formulrio estan vacios
     if(document.getElementById('name').value.length == 0 || document.getElementById('age').value.length == 0 || document.getElementById('gender').value.length == 0 || document.getElementById('address').value.length == 0 || document.getElementById('country').value.length == 0){
+        //agrega el attributo que desabilita el boton
         document.getElementById('button_info').disabled=true
     }else{
         document.getElementById('button_info').disabled=false
         console.log(document.getElementById('name').value)
     }
 }
-
+//lee cada 5 seg la funcion
 setInterval(validate_info, 500);
 
 
-
+//variable de el input state
 const $select = document.querySelector("#state");
 
 
-
-const opcionCambiada = () => {
-  console.log("Cambio");
-};
-
-$select.addEventListener("change", opcionCambiada);
-
+//constructor que agrega los datos
 const agregar_MX_AC = () => {
   const option = document.createElement('option');
   const valor = 'Aguas Calientes';
--- index.php
+++ index.php
@@ -7,50 +7,68 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Datos Generales</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 
-<body>
-    <legend>
-        <form action="controlador/datos_generales.php" method="POST">
-            <ul class="formulario">
-                <ol>
-                    <label for="name">Nombre:</label>
-                    <input type="text" id="name" name="name" require> 
-                </ol>
-                <ol>
-                    <label for="age">Edad:</label>
-                    <input type="text" id="age" name="age" require>
-                </ol>
-                <ol>
-                    <label for="gender">Genero:</label>
-                    <input type="text" id="gender" name="gender" require>
-                </ol>
-                <ol>
-                    <label for="address">Direccion:</label>
-                    <input type="text" id="address" name="address" require>
-                </ol>
-                <ol>
-                    <label for="country">Pais:</label>
-                    <select name="country" id="country">
-                        <option value="">Elige tu pais</option>
-                        <option value="MX">Mexico</option>
-                        <option value="EUA">Estados Unidos</option>
-                    </select>
-                </ol>
-                <ol>
-                    <label for="state">Estado:</label>
-                    <select name="state" id="state">
-                        <option value="">Apareceran los estados dependiendo del pais elegido</option>
-                    </select>
-                </ol>
-            </ul>
-            <button type="submit" id="button_info" class="boton">Enviar datos</button>
-        </form>
+<body class="m-0 vh-100 row justify-content-center align-items-center">
+    
+    <div class="container col-center p-5 ">
+        <div class="card" style="width: 40rem;">
+            <div class="card-body">
+                <div class="form-group">
+                    <form action="controlador/datos_generales.php" method="POST">
+                        <ul class="formulario">
+                            <ol>
+                                <label for="name">Nombre:</label>
+                                <input type="text" id="name" name="name" require class="form-control" aria-label="Text input with segmented dropdown button">
+                            </ol>
+                            <ol>
+                                <label for="age">Edad:</label>
+                                <input type="number" id="age" name="age" require class="form-control" aria-label="Text input with segmented dropdown button">
+                            </ol>
+                            <ol>
+                                <label for="gender">Genero:</label>
+                                <input type="text" id="gender" name="gender" require class="form-control" aria-label="Text input with segmented dropdown button">
+                            </ol>
+                            <ol>
+                                <label for="address">Direccion:</label>
+                                <input type="email" id="address" name="address" require class="form-control" aria-label="Text input with segmented dropdown button">
+                            </ol>
+                            <ol>
+                                <label for="country">Pais:</label>
+                                <select name="country" id="country" class="custom-select">
+                                    <option value="">Elige tu pais</option>
+                                    <option value="MX">Mexico</option>
+                                    <option value="EUA">Estados Unidos</option>
+                                </select>
+                            </ol>
+                            <ol>
+                                <label for="state">Estado:</label>
+                                <select name="state" id="state" class="custom-select">
+                                    <option value="">Apareceran los estados dependiendo del pais elegido</option>
+                                </select>
+                            </ol>
+                        </ul>
+                        <button type="submit" id="button_info" type="button" class="btn btn-info">Enviar datos</button>
+                    </form>
+                </div>
 
-    </legend>
+            </div>
+        </div>
+
+
+    </div>
+
 
 
     <script src="controller/controller_info.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
+    </script>
 
 </body>
 
